08a1c11a8bd3468f684dfe24402a9f67f41edbe1
花田奈々 <naa25-w.k@softbank.ne.jp>
2016-02-01 22:57:34 +0900

    Initial Commit

---
+++ count3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  count3
+//
+//  Created by 花田奈々 on 2016/02/01.
+//  Copyright © 2016年 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
