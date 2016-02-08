4eada02d2c97c04a945db93641aafebcde549d46
花田奈々 <chaminana.25@gmail.com>
2016-02-08 22:59:04 +0900

    first commit

--- count3/ViewController.swift
+++ count3/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var number: Int = 0
+   
+    @IBOutlet weak var temparatureLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    private func changeBackGround() {
+        backgroundImageView.image = UIImage(named: "sunn")
+    }
+    
+    @IBAction func minus(){
+        number = number-1
+        temparatureLabel.text = String(number)
+        
+        if number < 0 {
+            temparatureLabel.textColor = UIColor.blueColor()
+            backgroundImageView.image = UIImage(named: "yuki")
+            
+        }
+     }
+    
+    @IBAction func plus(){
+        number = number+1
+        temparatureLabel.text = String(number)
+        if number >= 0 {
+            temparatureLabel.textColor = UIColor.redColor()
+            backgroundImageView.image = UIImage(named: "sunn")
+        }
+        
+    }
 }
+
 
